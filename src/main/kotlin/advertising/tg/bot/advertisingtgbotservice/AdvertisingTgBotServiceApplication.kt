2843cd8858cdacf20b3cefa6f5da1244ae723243
SerjUstin <s.ustin.92@mail.ru>
2025-09-23 12:28:49 +0300

    Initial commit

---
+++ src/main/kotlin/advertising/tg/bot/advertisingtgbotservice/AdvertisingTgBotServiceApplication.kt
@@ -0,0 +1,11 @@
+package advertising.tg.bot.advertisingtgbotservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AdvertisingTgBotServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<AdvertisingTgBotServiceApplication>(*args)
+}
---
+++ src/test/kotlin/advertising/tg/bot/advertisingtgbotservice/AdvertisingTgBotServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package advertising.tg.bot.advertisingtgbotservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AdvertisingTgBotServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
